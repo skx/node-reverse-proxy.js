8aed14c3b41d790aa034c322cbcee1a20a83a786
Steve Kemp <steve@steve.org.uk>
2011-03-21 19:11:31 +0000

    Updated comments significantly

--- node-reverse-proxy.js
+++ node-reverse-proxy.js
@@ -37,7 +37,6 @@
  *
  */
 
-
 /**
  * Load the Node.js HTTP library.
  */
@@ -45,11 +44,11 @@ var http = require('http');
 var path = require('path');
 
 
+
 /**
  * Debug?
  */
 var g_debug = false;
-
 
 /**
  * Our global re-write rules.
@@ -59,8 +58,6 @@ var g_debug = false;
  */
 var global;
 
-
-
 /**
  * Global cache of pre-compiled regular expression objects for path-based
  * rewrites.
@@ -69,11 +66,13 @@ var global;
  */
 var re_Rewrites = {};
 
-
 /**
  * Global cache of pre-compiled regular expressions for our known hostnames.
  */
 var re_Hosts = {};
+
+
+
 
 
 
@@ -83,7 +82,7 @@ var re_Hosts = {};
  * requests and proxy/rewrite them.
  *
  */
-var handler = function (req, res) {
+var handler = function(req, res) {
 
     /**
      * Access the Host: which we received, ensuring it is
@@ -94,17 +93,23 @@ var handler = function (req, res) {
 
 
     /**
-     * Loggy
+     * Log, if being verbose.
      */
     if (g_debug) {
         console.log("Request for " + vhost + req.url + " from " + req.connection.remoteAddress);
     }
 
-    if ( req.url.substr(0,2) == "//" )
-    {
+
+    /**
+     * This is thttpd specific, but it seems that requests with two
+     * leading "/" characters result in a "400 bad request" response.
+     */
+    if (req.url.substr(0, 2) == "//") {
         console.log("Request for BOGUS URL [http://" + vhost + req.url + "] from " + req.connection.remoteAddress);
         req.url = req.url.substr(1);
     }
+
+
     /**
      * If there is a port in the vhost-name then we'll drop it.
      */
@@ -113,9 +118,10 @@ var handler = function (req, res) {
         vhost = vhost.substr(0, port);
     }
 
+
     /**
-     * Save away the original submitted Host: header, this might be useful
-     * for one of our functional hooks.
+     * Save away the original submitted Host: header, this will be passed
+     * to any functional hooks which might be present for this virtual host.
      */
     var orig_vhost = vhost;
 
@@ -128,7 +134,10 @@ var handler = function (req, res) {
 
 
     /**
-     * This lets us use our vhosts as regexps.  We might not want that?
+     * Find the virtual host, from our external table.
+     *
+     * Note that the entries in that table are regular expressions, albeit
+     * anchored ones.
      */
     for (var host in global.options) {
         var hostRE = re_Hosts[host];
@@ -140,15 +149,18 @@ var handler = function (req, res) {
 
 
     /**
-     * If that succeeded it is a host we know about.
+     * If the table lookup succeeded then we have a request for a virtual
+     * host we know about.
      */
     if (ent) {
+
         /**
          * Find any rewrite-rules which might be present for this vhost.
          */
         var rules = global.options[vhost]['rules'];
+
         if (rules) {
-            Object.keys(rules).forEach(function (rule) {
+            Object.keys(rules).forEach(function(rule) {
 
                 /**
                  * Find the pre-compiled regexp for this rule - execute it.
@@ -162,15 +174,16 @@ var handler = function (req, res) {
                      * If the rule matches we have a hit; we need
                      * to rewrite.
                      */
-                    if (g_debug) {
-                        console.log("\tRewrite '" + rule + "' -> '" + rules[rule] + "'");
-                    }
-
                     var newURL = rules[rule];
 
                     /**
-                     * If we have a positive number of matches (i.e. "captures") then we need to search and replace
+                     * If we have a positive number of matches
+                     * (i.e. "captures") then we need to search and replace
                      * them in turn.
+                     *
+                     *  So we replace $1 with the first capture,
+                     * we replace $2 with the second capture, etc.
+                     *
                      */
                     if (match.length > 1) {
                         var i = 1;
@@ -179,6 +192,7 @@ var handler = function (req, res) {
                             i = i + 1;
                         }
                     }
+
 
                     /**
                      * If the destination rule begins with
@@ -190,9 +204,10 @@ var handler = function (req, res) {
                         });
                         res.end();
                     } else {
+
                         /**
-                         * Otherwise we'll update the request - on the basis that we're going to proxy
-                         * it shortly...
+                         * Otherwise we'll update the request - on the basis
+                         * that we're going to proxy it shortly.
                          */
                         req.url = newURL;
                     }
@@ -202,27 +217,37 @@ var handler = function (req, res) {
 
 
         /**
-         * Now a functional modifier, which will be fun.
+         * See if we have any modification functions to invoke.
+         *
+         * If there are we execute each one in turn.  If we receive
+         * any true response we will regard the request as over
+         * and will not continue to proxy that request.
+         *
+         * The functions are invoked based upon the URL-path requested,
+         * but it is possible there will be more than one.
          */
         var func = global.options[vhost]['functions'];
         var over = false;
 
         if (func) {
 
-            Object.keys(func).forEach(function (fun) {
+            Object.keys(func).forEach(function(fun) {
 
-                if ( ! over )
-                {
-                    /* The name of the function is a regexp against the path. */
-                    if (req.url.match(fun)) {
-                        over = global.options[vhost]['functions'][fun](orig_vhost, vhost, req, res);
+                /* The name of the function is a regexp against the path. */
+                if (req.url.match(fun)) {
+
+                    if ( global.options[vhost]['functions'][fun](orig_vhost, vhost, req, res) ) {
+                        over = true;
                     }
                 }
             })
         }
 
-        if ( over )
-        {
+        /**
+         * If (at least one) functional hook returned true then we're
+         * done with this request.
+         */
+        if (over) {
             return;
         }
 
@@ -230,15 +255,16 @@ var handler = function (req, res) {
         /**
          * OK at this point we have either:
          *
-         *  a.  No rewrite rules defined for this vhost.
+         *  a.  No rewrite rules/functions defined for this vhost.
          *
-         *  b.  Rules defined, but with no matches having been made.
+         *  b.  Rules defined, but with no matches having been made, or
+         *     without a functional hook returning "true".
          *
          * Lookup the host+port we're going to proxy to.
          */
-
         port = global.options[vhost]['port'];
         host = global.options[vhost]['host'] || "127.0.0.1";
+
 
         /**
          * If that lookup fails we're out of luck.
@@ -251,6 +277,7 @@ var handler = function (req, res) {
             return;
         }
 
+
         /**
          * Otherwise we need to create the proxy-magic.
          */
@@ -259,7 +286,7 @@ var handler = function (req, res) {
         /**
          * The proxied connection might fail.
          */
-        proxy.addListener('error', function (socketException) {
+        proxy.addListener('error', function(socketException) {
             console.log("Request for " + vhost + " failed - back-end server " + host + ":" + port + " unreachable");
             res.writeHead(503, {
                 'content-type': 'text/html'
@@ -273,19 +300,25 @@ var handler = function (req, res) {
          * X-Forwarded-For: header.
          */
         var agent = ""
-        if ( req.headers["user-agent"] ) {
+        if (req.headers["user-agent"]) {
             agent = req.headers["user-agent"] + "; ";
         }
         req.headers["X-Forwarded-For"] = req.connection.remoteAddress;
-        req.headers["User-Agent"]      = agent + "node-reverse-proxy.js";
+        req.headers["User-Agent"] = agent + "node-reverse-proxy.js";
+
 
         /**
          * Create the proxier
          */
         var proxy_request = proxy.request(req.method, req.url, req.headers);
 
-        proxy_request.addListener('response', function (proxy_response) {
+        proxy_request.addListener('response', function(proxy_response) {
 
+            /**
+             * If we have a "Connection: foo" header preserve it.
+             *
+             * Otherwise defualt to "close".
+             */
             if (proxy_response.headers.connection) {
                 if (req.headers.connection)
                     proxy_response.headers.connection = req.headers.connection;
@@ -295,31 +328,37 @@ var handler = function (req, res) {
 
             res.writeHead(proxy_response.statusCode, proxy_response.headers);
 
-            // No 'data' event and no 'end'
+            /**
+             * No 'data' event and no 'end'
+             *
+             * Missing this will lead to oddness - don't ask.
+             */
             if (proxy_response.statusCode === 304) {
                 res.end();
                 return;
             }
 
-            proxy_response.addListener('data', function (chunk) {
+            proxy_response.addListener('data', function(chunk) {
                 res.write(chunk, 'binary');
             });
-            proxy_response.addListener('end', function () {
+            proxy_response.addListener('end', function() {
                 res.end();
             });
 
         });
 
+
         /**
          * Wire it all up, old-school.
          */
-        req.addListener('data', function (chunk) {
+        req.addListener('data', function(chunk) {
             proxy_request.write(chunk, 'binary');
         });
 
-        req.addListener('end', function () {
+        req.addListener('end', function() {
             proxy_request.end();
         });
+
 
         /**
          * Our work here is done.
@@ -340,17 +379,13 @@ var handler = function (req, res) {
 
 
 
+
 /**
  * Last ditch error-recovery
  */
-process.on('uncaughtException', function (err) {
+process.on('uncaughtException', function(err) {
     console.log("ERROR:" + err);
 });
-
-
-
-
-
 
 
 
@@ -361,13 +396,14 @@ process.on('uncaughtException', function (err) {
 var file = "./rewrites.js";
 
 
+
 /**
  * Simple command-line parsing.
  */
 var inFile = false;
 var inPort = false;
 
-process.argv.forEach(function (arg) {
+process.argv.forEach(function(arg) {
 
     if (inFile) {
         file = arg;
@@ -392,6 +428,8 @@ process.argv.forEach(function (arg) {
     }
 })
 
+
+
 /**
  * Ensure we weren't left dangling.
  */
@@ -401,8 +439,10 @@ if (inFile || inPort) {
 }
 
 
+
+
 /**
- * See if our file exists.
+ * See if our named configuration file exists.
  */
 if (path.existsSync(file)) {
     global = require(file);
@@ -412,7 +452,6 @@ if (path.existsSync(file)) {
 }
 
 
-
 /**
  * Pre-compile each rewrite rule regular expression, and each vhost
  * regexp.
@@ -420,7 +459,7 @@ if (path.existsSync(file)) {
  * Doing this offers a significant speedup.
  *
  */
-Object.keys(global.options).forEach(function (vhost) {
+Object.keys(global.options).forEach(function(vhost) {
 
     /**
      * Virtual Hostname regexp.
@@ -432,17 +471,21 @@ Object.keys(global.options).forEach(function (vhost) {
      */
     rules = global.options[vhost]['rules'];
     if (rules) {
-        Object.keys(rules).forEach(function (rule) {
+        Object.keys(rules).forEach(function(rule) {
             re_Rewrites[rule] = new RegExp(rule);
         })
     }
 })
 
+
+
+
 /**
- * Entry point - start our server up on the port & addresses we've got
- * defined.
+ * Launch our starting options.
  */
 console.log("node-reverse-proxy.js starting, reading from " + file + "\n");
+
+
 
 
 /**
@@ -451,13 +494,16 @@ console.log("node-reverse-proxy.js starting, reading from " + file + "\n");
  */
 var port = port || global.port;
 
+
+
 /**
- * Bind to each requested address, defined in the configuration file.
+ * Bind to each requested address, as defined in the configuration file.
  */
 for (val in global.bind) {
     console.log("Binding to " + global.bind[val] + ":" + port);
     http.createServer().addListener("request", handler).listen(port, global.bind[val]);
 }
+
 
 
 /**
